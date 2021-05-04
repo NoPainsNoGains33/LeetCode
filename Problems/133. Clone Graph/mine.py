906091b57d654add2f13d25607d229c34ec345ed
Yushen Ni <49766929+NoPainsNoGains33@users.noreply.github.com>
2021-05-04 07:41:52 -0400

    O(N+M), O(N+M)

---
+++ Problems/133. Clone Graph/mine.py
@@ -0,0 +1,48 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+
+        all_nodes = self.visitGraph(node)
+        node_mapping = self.copyNodes(all_nodes)
+        self.copyEdges(all_nodes, node_mapping)
+        
+        return node_mapping[node]
+    
+    def visitGraph(self, node):
+        queue = collections.deque([node])
+        visited = set([node])
+        
+        while queue:
+            current_node = queue.popleft()
+            for neighbor in current_node.neighbors:
+                if neighbor in visited:
+                    continue
+                visited.add(neighbor)
+                queue.append(neighbor)
+        
+        return list(visited)
+    
+    def copyNodes(self, all_nodes):
+        node_mapping = {}
+        for node in all_nodes:
+            node_mapping[node] = Node(node.val)
+            
+        return node_mapping
+    
+    def copyEdges(self, all_nodes, node_mapping):
+        for node in all_nodes:
+            new_node = node_mapping[node]
+            for neighbor in node.neighbors:
+                new_neighbor = node_mapping[neighbor]
+                new_node.neighbors.append(new_neighbor)
+        
