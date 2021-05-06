8bea6b093a28f3c133169e2ebc8b2c95a3780a84
Yushen Ni <49766929+NoPainsNoGains33@users.noreply.github.com>
2021-05-06 08:21:57 -0400

    O(N + M), O(N)

---
+++ Problems/Lint. 127 · Topological Sorting/solution1.py
@@ -0,0 +1,37 @@
+"""
+class DirectedGraphNode:
+     def __init__(self, x):
+         self.label = x
+         self.neighbors = []
+"""
+
+class Solution:
+    """
+    @param graph: A list of Directed graph node
+    @return: Any topological order for the given graph.
+    """
+    def topSort(self, graph):
+        # write your code here
+        node_to_indegree = self.getIndegree(graph)
+        start_nodes = [node for node in graph if node_to_indegree[node] == 0]
+        # start_nodes = [node for node, indegree in node_to_indegree if indegree == 0]
+        queue = collections.deque(start_nodes)
+
+        order = []
+        while queue:
+            current = queue.popleft()
+            order.append(current)
+            for neighbor in current.neighbors:
+                node_to_indegree[neighbor] -= 1
+                if node_to_indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return order
+
+    def getIndegree(self, graph):
+        node_to_indegree = {node : 0 for node in graph}
+        for node in graph:
+            for neighbor in node.neighbors:
+                node_to_indegree[neighbor] += 1
+        
+        return node_to_indegree
